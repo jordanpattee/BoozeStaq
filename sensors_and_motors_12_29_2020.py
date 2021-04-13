96b62c0cbecca784b2d8cabdd7fced90fbb25027
Kennedy242 <80504103+Kennedy242@users.noreply.github.com>
2021-04-13 12:33:09 -0600

    Update sensors_and_motors_12_29_2020.py
    Updated Code from Kayce

--- sensors_and_motors_12_29_2020.py
+++ sensors_and_motors_12_29_2020.py
@@ -3,17 +3,12 @@
 """
 Created on Tue Dec 29 10:58:47 2020
 
-@author: pi
+@author: Kayce Serafin
 """
 
-
-import pandas as pd
-from gpiozero import LED
 from time import sleep
-import time
-import re
+import RPi.GPIO as GPIO
 import numpy as np
-
 
 #1) self.percents[i] will be between 0-100 
 
@@ -23,32 +18,120 @@ import numpy as np
 #3)The index of the holder/ingredients starts from 0 [0,1,2,3,4,5]),
 #so self.percents[0] will be the percent associated with ingredient 0 and holder 0.
 
-#4)sizes are be as follows: 0=shot, 1=regular, 2=batch
+#4)sizes are be as follows: 0=regular, 1=batch
 #self.size only returns one value
-# if shot is selected, self.size = 0
-
-
 
 class sensors_and_motors(object): 
     def _init_(self):
         self.percentages = gm.self.percentages # percents will be between (0-100)
-        self.size = gm.self.size #size (0=shot,1=regular, 2 = batch)
+        self.size = gm.self.size #size (0=regular, 1 = batch)
         
     def pour_drink(self):
-        #LED code for an example
-        I1 = LED(12)
-        I2 = LED(17)
-        I3 = LED(15)
-        I4 = LED(18)
-        I5 = LED(19)
-        I6 = LED(20)
+        pulseHorizontal = 17  # Stepper Drive Pulses
+        directionHorizontal = 27  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
+        enableHorizontal = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
+        pulseVertical = 10
+        directionVertical = 9
+        enableVertical = 11
+
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setwarnings(False)
+
+        GPIO.setup(pulseHorizontal, GPIO.OUT)
+        GPIO.setup(directionHorizontal, GPIO.OUT)
+        GPIO.setup(enableHorizontal, GPIO.OUT)
+        GPIO.setup(pulseVertical, GPIO.OUT)
+        GPIO.setup(directionVertical, GPIO.OUT)
+        GPIO.setup(enableVertical, GPIO.OUT)
+
+        delay = 0.0000001 # This is actually a delay between PUL pulses - effectively sets the motor rotation speed.
+
+        def left(durationLeft):
+            GPIO.output(enableHorizontal, GPIO.HIGH)    
+            sleep(.5) # pause due to a possible change direction
+            GPIO.output(directionHorizontal, GPIO.LOW)  
+            for x in range(durationLeft): 
+                GPIO.output(pulseHorizontal, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(pulseHorizontal, GPIO.LOW)
+                sleep(delay)
+            GPIO.output(enableHorizontal, GPIO.LOW)
+            sleep(.5) # pause for possible change direction
+            return
+
+        def right(durationRight):
+            GPIO.output(enableHorizontal, GPIO.HIGH)    
+            sleep(.5) # pause due to a possible change direction
+            GPIO.output(directionHorizontal, GPIO.HIGH)
+            for y in range(durationRight):
+                GPIO.output(pulseHorizontal, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(pulseHorizontal, GPIO.LOW)
+                sleep(delay)
+            GPIO.output(enableHorizontal, GPIO.LOW)
+            sleep(.5) # pause for possible change direction
+            return
+
+        def down(durationDown):
+            GPIO.output(enableVertical, GPIO.HIGH)
+            sleep(.5) # pause due to a possible change direction
+            GPIO.output(directionVertical, GPIO.LOW)
+            for x in range(durationDown): 
+                GPIO.output(pulseVertical, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(pulseVertical, GPIO.LOW)
+                sleep(delay)
+            GPIO.output(enableVertical, GPIO.LOW)
+            sleep(.5) # pause for possible change direction
+            return
+
+        def up(durationUp):
+            GPIO.output(enableVertical, GPIO.HIGH)
+            sleep(.5) # pause due to a possible change direction
+            GPIO.output(directionVertical, GPIO.HIGH)
+            for y in range(durationUp):
+                GPIO.output(pulseVertical, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(pulseVertical, GPIO.LOW)
+                sleep(delay)
+            GPIO.output(enableVertical, GPIO.LOW)
+            sleep(.5) # pause for possible change direction
+            return
         
-        I = [I1,I2,I3,I4,I5,I6]
-        print('percentages (index of element is bottle holder)=',self.percentages)
+        def dispense():
+            up(dispenseVal)
+            sleep(1.5)
+            down(dispenseVal)                 
         
-        for i in range(len(self.percentages)):
-                I[i].on()
-                sleep(self.percentages[i]/10)
-                
-                I[i].off()
-                
+        #print('percentages (index of element is bottle holder)=',self.percentages)
+        bottles = np.count_nonzero(self.percentages)
+        size = self.size        
+        positions = 1
+        loopcount = 0
+        betweenBH = 8000
+        dispenseVal = 4950
+        toBH1 = 250
+        #go to BH1
+        right(toBH1)
+        while bottles >= positions: #while we havn't been to every bottle we need to dispense from
+            if loopcount != 0: #on first loop iteration, no need to move right, we are already at BH1
+                right(betweenBH)
+            if self.percentages[loopcount] != 0: #if ingredient is not zero
+                if self.percentages[loopcount] < 10: # to avoid rounding an ingredient to zero
+                    self.percentages[loopcount] = 10
+                if self.size: #batch size 
+                    for i in range( int(round(self.percentages[loopcount]/10))+1 ):
+                        dispense()
+                    positions = positions + 1
+                else: #regular size
+                    for i in range( int(round(self.percentages[loopcount]/10)) ):
+                        dispense()
+                    positions = positions + 1
+            loopcount = loopcount + 1
+        toHome = betweenBH*(loopcount - 1) + toBH1  #claculate steps returning to starting position      
+        left(toHome) #return to start position
+            
+        GPIO.cleanup()
+        GPIO.cleanup()
+            
